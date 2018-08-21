ff9cc30e4218693924ee3e4bc1d43e64852fc5e1
intersignature <intersignature@hotmail.com>
2018-08-21 11:08:39 +0700

    initial commit

---
+++ TrainMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainMe
+//
+//  Created by Sirichai Binchai on 21/8/2561 BE.
+//  Copyright © 2561 Sirichai Binchai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
