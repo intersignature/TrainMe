a780a55e780f07298d56269d8ea2de56f547c24f
intersignature <intersignature@hotmail.com>
2018-12-19 16:43:31 +0700

    Add change schedule request by trainee

--- TrainMe/trainee/controllers/EachOngoingTraineeViewController.swift
+++ TrainMe/trainee/controllers/EachOngoingTraineeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import FirebaseAuth
-
+import FirebaseDatabase
 
 class EachOngoingTraineeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -16,12 +16,14 @@ class EachOngoingTraineeViewController: UIViewController, UITableViewDataSource,
     
     var selectedOngoing: OngoingDetail!
     var currentUser: User!
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("EachOngoingTraineeViewController \(self.selectedOngoing)")
         
+        self.ref = Database.database().reference()
         self.currentUser = Auth.auth().currentUser
         
         self.eachOngoingScheduleTableView.dataSource = self
@@ -63,13 +65,33 @@ class EachOngoingTraineeViewController: UIViewController, UITableViewDataSource,
     
     @objc func requestScheduleBtnAction(sender: UIButton) {
         
-        let alert = UIAlertController(title: "Confirm to request change schedule?", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Confirm to request change schedule?", message: "from \(self.selectedOngoing.eachOngoingDetails[sender.tag].start_train_date) \(self.selectedOngoing.eachOngoingDetails[sender.tag].start_train_time)", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action) in
+            
             print("Confirm to request")
             //TODO: request
+            self.changeStatusToRequestChangeSchedule(changeIndex: sender.tag)
         }))
         alert.addAction(UIAlertAction(title: "No", style: .destructive, handler: nil))
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    func changeStatusToRequestChangeSchedule(changeIndex: Int) {
+        
+        let changeData = ["status": "3"]
+        self.ref.child("progress_schedule_detail").child(self.selectedOngoing.trainerId).child(self.currentUser.uid).child(self.selectedOngoing.ongoingId).child(self.selectedOngoing.eachOngoingDetails[changeIndex].count).updateChildValues(changeData) { (err, ref) in
+            
+            if let err = err {
+                print(err.localizedDescription)
+                self.createAlert(alertTitle: err.localizedDescription, alertMessage: "")
+                return
+            }
+            
+            self.selectedOngoing.eachOngoingDetails[changeIndex].status = "3"
+            self.eachOngoingScheduleTableView.reloadData()
+            self.createAlert(alertTitle: "Request to change schedule successful", alertMessage: "Please wait for your trainer change schedule")
+        }
+        
     }
     
     @objc func reviewBtnAction(sender: UIButton) {
