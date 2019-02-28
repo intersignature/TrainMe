2990b508658ad1a42221e8ad20fc1ba80f7be6d0
intersignature <intersignature@hotmail.com>
2019-02-28 14:13:11 +0700

    Change View help to new UI

--- TrainMe/ViewHelpViewController.swift
+++ TrainMe/ViewHelpViewController.swift
@@ -26,10 +26,20 @@ class ViewHelpViewController: UIViewController {
         self.helpTv.scrollRangeToVisible(NSRange(location:0, length:0))
         self.helpTv.text = self.selectedHelp.desc
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.setupNavigationStyle()
+//        self.setupNavigationStyle()
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
+        
+        self.helpTv.backgroundColor = UIColor.white.withAlphaComponent(0.4)
+        self.helpTv.layer.cornerRadius = 5
     }
     
     @IBAction func cancelBtnAction(_ sender: UIBarButtonItem) {
