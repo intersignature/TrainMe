40ab5c8fdb72ae17044d438bae75130e755bb302
intersignature <intersignature@hotmail.com>
2019-03-07 15:15:03 +0700

    Use tableView in  setting page

--- TrainMe/SettingTableViewController.swift
+++ TrainMe/SettingTableViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingTableViewController: UITableViewController {
 
-    @IBOutlet weak var langSw: UISwitch!
+    @IBOutlet weak var currentLang: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -20,17 +21,34 @@ class SettingViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func langSwOb(_ sender: UISwitch) {
-        print(sender.isOn)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 && indexPath.section == 0 {
+            print(Locale.preferredLanguages[0])
+        }
+    }
+    
+    func checkCurrentLang() {
+        
+        if Locale.preferredLanguages[0].components(separatedBy: "-").first == "en" {
+            self.currentLang.text = "EN"
+        } else if Locale.preferredLanguages[0].components(separatedBy: "-").first == "th" {
+            self.currentLang.text = "TH"
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        tableView.tableFooterView = UIView()
+        tableView.backgroundView = UIImageView(image: UIImage(named: "BG_HOME"))
         
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
         self.navigationController?.view.backgroundColor = .clear
+        
+        self.checkCurrentLang()
     }
 }
